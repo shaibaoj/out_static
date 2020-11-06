4b70e6774d801928e2cf9ece9c957ac30a567b2a
shaibaoj <987939309@qq.com>
2020-11-06 19:27:08 +0800

    no message

--- static/share/js/components.js
+++ static/share/js/components.js
@@ -39,6 +39,236 @@ var vmminxCommonData = {
     }
 };
 
+
+var vmminxAppData = {
+    data: {
+        app_url: '/app.app.view',
+        app: {
+            detail: {},
+            jump_url: '',
+            downloads: 0,
+            notice: false,
+            show: false,
+            btnText: '一键复制',
+            copyStatus: false
+        }
+    },
+    methods: {
+        queryApp: function () {
+            var $this = this;
+            ajaxPost(URLPrefix.baseUrl + $this.app_url, {}, function (result) {
+                if (result.code == 0 && result.data.detail) {
+                    $this.app.detail = result.data.detail;
+                    $this.show = 1;
+
+                    var androidUrl = result.data.detail.android_url;
+                    var iosUrl = result.data.detail.ios_url;
+                    if (URLPrefix.ua.match(/iphone/i) == "iphone" || URLPrefix.ua.match(/ipad/i) == "ipad") {
+                        $this.app.jump_url = iosUrl;
+                    } else {
+                        $this.app.jump_url = androidUrl;
+                    }
+                }
+            });
+        },
+        showApp() {
+            this.app.show = true;
+        },
+        hideApp() {
+            this.app.show = false;
+        },
+        downloads_url() {
+            var $this = this;
+            if (mobileUtil.isIOS && mobileUtil.isWeixin) {
+                location.href = $this.app.jump_url;
+            } else if (mobileUtil.isAndroid && mobileUtil.isWeixin && $this.app.jump_url.indexOf("app.qq.com") <= 0) {
+                $this.app.notice = true;
+            } else {
+                location.href = $this.app.jump_url;
+            }
+        },
+        appCopyContent() {
+            var $this = this;
+            var clipboard = new ClipboardJS('.appCopy', {
+                target: function () {
+                    return document.querySelector('.shareapp-code');
+                }
+            });
+            clipboard.on('success', function (e) {
+                layer.msg('复制成功', {
+                    time: 1500
+                });
+                e.clearSelection();
+                clipboard.destroy();
+                $this.app.btnText = '复制成功';
+                $this.app.copyStatus = true;
+                setTimeout(() => {
+                    $this.app.btnText = '一键复制';
+                    $this.app.copyStatus = false;
+                }, 2000);
+            });
+            clipboard.on('error', function (e) {
+                layer.msg('由于您的浏览器不兼容或当前网速较慢，复制失败，请手动复制或更换主流浏览器！', {
+                    icon: 2
+                });
+                $this.app.btnText = '复制失败';
+            });
+        },
+        get_downloads() {
+            var $this = this;
+            ajaxPost(URLPrefix.baseUrl + "/app.app.downloads", {}, function (result) {
+                if (result.info.status == 0) {
+                    $this.app.downloads = result.data.downloads;
+                    setTimeout(function () {
+                        $this.get_downloads();
+                    }, 1000);
+                }
+            });
+        },
+        hideNotice() {
+            this.app.notice = !this.app.notice;
+        },
+    }
+};
+
+
+
+Vue.component('component-app', {
+    props: ['app'],
+    data: function () {
+        return {
+        }
+    },
+    mounted: function () {
+    },
+    methods: {
+        showApp: function () {
+            this.$emit("show-app");
+        },
+    },
+    template:
+        '<div class="app-download">' +
+        '<div class= "app-icon">' +
+        '<img v-if="app.detail.icon_url != \'\'" :src="app.detail.icon_url">' +
+        '</div>' +
+        '<div class="app-content">' +
+        '<div class="app-title">{{ app.detail.name }}</div>' +
+        '<div class="app-desc">{{ app.detail.description }}</div>' +
+        '</div>' +
+        '<div class="app-download-buttons" @click="showApp" >' +
+        '<p class="app-download-botton">' +
+        '<span>下载APP</span>' +
+        '</p>' +
+        '</div >' +
+        '</div > '
+});
+
+
+Vue.component('component-app-bar', {
+    props: ['app'],
+    data: function () {
+        return {
+
+        }
+    },
+    methods: {
+        showApp: function () {
+            this.$emit("show-app");
+        },
+    },
+    template:
+        '<div class="app-bar">' +
+        '<div class="app-bar-content">' +
+        '<div class="app-title">{{ app.detail.name }}</div>' +
+        '<div class="app-desc">{{ app.detail.description }}</div>' +
+        '</div>' +
+        '<div class="app-download-buttons" @click="showApp">' +
+        '<p class="app-download-botton">' +
+        '<span>下载APP</span>' +
+        '</p>' +
+        '</div>' +
+        '</div>'
+});
+
+Vue.component('component-app-dialog', {
+    props: ['app'],
+    data: function () {
+        return {
+        }
+    },
+    mounted: function () {
+    },
+    methods: {
+        hideApp() {
+            this.$emit("hide-app");
+        },
+        downloads_url() {
+            var $this = this;
+            if (mobileUtil.isIOS && mobileUtil.isWeixin) {
+                location.href = $this.app.jump_url;
+            } else if (mobileUtil.isAndroid && mobileUtil.isWeixin && $this.app.jump_url.indexOf("app.qq.com") <= 0) {
+                $this.app.notice = true;
+            } else {
+                location.href = $this.app.jump_url;
+            }
+        },
+        appCopyContent() {
+            var $this = this;
+            var clipboard = new ClipboardJS('.appCopy', {
+                target: function () {
+                    return document.querySelector('.shareapp-code');
+                }
+            });
+            clipboard.on('success', function (e) {
+                layer.msg('复制成功', {
+                    time: 1500
+                });
+                e.clearSelection();
+                clipboard.destroy();
+                $this.app.btnText = '复制成功';
+                $this.app.copyStatus = true;
+                setTimeout(() => {
+                    $this.app.btnText = '一键复制';
+                    $this.app.copyStatus = false;
+                }, 2000);
+            });
+            clipboard.on('error', function (e) {
+                layer.msg('由于您的浏览器不兼容或当前网速较慢，复制失败，请手动复制或更换主流浏览器！', {
+                    icon: 2
+                });
+                $this.app.btnText = '复制失败';
+            });
+        },
+    },
+    template:
+        '<div class="app-box dialog-box">' +
+        '<div class="box-mask" @click="hideApp"></div>' +
+        '<div class="box-content">' +
+        '<div class="box-title">恭喜您</div>' +
+        '<div class="box-content">' +
+        '<p class="box-text">先复制邀请码</p>' +
+        '<div class="box-invite-code">' +
+        '<div class="copyBut flex-box">' +
+        '<p class="flex-two">邀请码：' +
+        ' <span class="shareapp-code">{{app.detail.invite_code}}</span>' +
+        '</p>' +
+        '<a :data-clipboard-text="app.detail.invite_code" href="javascript:;"' +
+        'class="flex-one appCopy" @click="appCopyContent">复制' +
+        '</a>' +
+        '</div>' +
+        '</div>' +
+        '<p class="box-text">再打开app，点击每日福利，即可领取福利商品</p>' +
+        '<div class="box-btn animation-btn" @click="downloads_url">立即打开APP，领取福利</div>' +
+        '<marquee direction="up" scrollamount="1" class="marquee">' +
+        '<span>186****1198 刚刚领取了商品</span>' +
+        '</marquee>' +
+        '</div>' +
+        '</div>' +
+        '</div>'
+});
+
+
+
 Vue.component('component-banner', {
     mixins: [vmminxCommonData],
     props: ['itemData'],
